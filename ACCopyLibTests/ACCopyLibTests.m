dd234f24cace3e75d67303fe4d75f0a8c38c5b15
aquachex <dave.lilly@gmail.com>
2013-09-27 10:28:45 -0500

    Initial Commit

---
+++ ACCopyLibTests/ACCopyLibTests.m
@@ -0,0 +1,34 @@
+//
+//  ACCopyLibTests.m
+//  ACCopyLibTests
+//
+//  Created by Dave Lilly on 9/27/13.
+//  Copyright (c) 2013 Dave Lilly. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ACCopyLibTests : XCTestCase
+
+@end
+
+@implementation ACCopyLibTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
